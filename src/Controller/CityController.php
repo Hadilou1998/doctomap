db830a96effee1bba0607e03c7675073e6be55f9
Hadilou1998 <hadilissilame@gmail.com>
2024-09-13 11:48:26 +0200

    Updated CityController.php

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\DoctorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CityController extends AbstractController
