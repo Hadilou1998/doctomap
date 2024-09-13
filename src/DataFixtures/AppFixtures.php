112f78b371770ce8c8c02d2a20167ac1ad16cfd0
Hadilou1998 <hadilissilame@gmail.com>
2024-09-13 11:00:30 +0200

    Updated project doctomap

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\DoctorRepository;
+use App\Entity\Doctor;
+use Faker\Factory;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 30; $i++) {
+            $doctor = new Doctor();
+            $doctor->setFirstname($faker->firstName());
+            $doctor->setLastname($faker->lastName());
+            $doctor->setSpeciality($faker->jobTitle());
+            $doctor->setAddress($faker->streetAddress());
+            $doctor->setCity($faker->city());
+            $doctor->setZip($faker->postcode());
+            $doctor->setPhone($faker->phoneNumber());
+
+            $manager->persist($doctor);
+        }
+
+        $manager->flush();
+    }
+}
--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DoctorRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DoctorRepository::class)]
+#[ApiResource]
 class Doctor
 {
     #[ORM\Id]
